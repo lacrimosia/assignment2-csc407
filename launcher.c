101188a51c18190fc862ccd0298bcf440caa9794
Shatilla Prayer <shatilla.prayer@unlv.edu>
2019-05-08 23:27:08 -0700

    Added more functions

--- launcher.c
+++ launcher.c
@@ -3,6 +3,7 @@
 #include	<signal.h>
 #include	<string.h>
 #include 	<unistd.h>
+//#include    "signaller.c"
 
 int counter = 0;
 int shouldContinue = 1;
@@ -10,48 +11,105 @@ int resetTimeInSec;  // read into command line
 int signalTimeInSec; // read into command line
 
 // constants
-#define MIN_TIME_IN_SECS = 2;
+const int MIN_TIME_IN_SECS = 2;
 
 void sigAlarmHandler(int sig){
   counter = 0;
+  // TODO
+  // look at resetting 
+  // resetting to receive SIGALARM resetTimeInSec 
   resetTimeInSec = 0;
+  printf("Resetting \n");
 }
 
 void sigUSR1Handler(int sig){
+	printf("Got %d \n", counter);
 	counter += 1;
-	if(counter >= 3)
+	
+
+	if(counter >= 3){
 		shouldContinue = 0;
+		printf("Got %d Have reached the limit!\n", counter);
+	}
 }
 
 int	main(int argc, char *argv[]){
 
-	resetTimeInSec = atoi(argv[1]);
-	signalTimeInSec = atoi(argv[2]);
+	if(argc != 3){
+		printf("Usage: launcher <resetTime> <signalTime>\n");
+		return(EXIT_FAILURE);
+	}
 
+	if(atoi(argv[1]) >= MIN_TIME_IN_SECS){
+		resetTimeInSec = atoi(argv[1]);
+	}else{
+		printf("resetTime must be %d or greater.\n", MIN_TIME_IN_SECS);
+		return(EXIT_FAILURE);
+	}
+
+	if(atoi(argv[2]) >= MIN_TIME_IN_SECS){
+		signalTimeInSec = atoi(argv[2]);
+	}else{
+		printf("signalTimeInSec must be %d or greater.\n", MIN_TIME_IN_SECS);
+		return(EXIT_FAILURE);
+	}
+	
+	
+	//TODO
+	// correctly installing 2 signal handlers
 	// set up the struct
 	// install the signal handler
 	struct sigaction act;
 	memset(&act, '\0', sizeof(act));
 	act.sa_handler = sigAlarmHandler;
-	signal(SIGINT, sigAlarmHandler);
+	//sigaction(SIGALRM,&act,NULL);
+	signal(SIGALRM, sigAlarmHandler);
 	act.sa_handler = sigUSR1Handler;
-	signal(SIGINT, sigUSR1Handler);
-
-	// making 3 child processes
-	// put the processes into the array
-	int processes[3];
-	int i=0;
-	for(i; i<3; i++){
-		fork();
-		processes[i] = getpid();
-		printf("signaller %d signalling parent\n", processes[i]);
-	}
-	//printf("[son] pid %d from [parent] pid %d\n",getpid(),getppid()); 
+	//sigaction(SIGUSR1,&act,NULL);
+	signal(SIGUSR1, sigUSR1Handler);
 
 	const int BUFFER_LEN = 64;
 	char buffer[BUFFER_LEN];
 
 	snprintf(buffer,BUFFER_LEN,"%d",signalTimeInSec);
+
+	// TODO
+	// making 3 children processes which use execl to call signaller with 
+	// signaltimeInSec
+
+	int processes[3];
+	int i=0;
+
+	for(i;i<3;i++) // loop will run n times (n=5) 
+    { 
+        if(fork() == 0) 
+        { 
+            //printf("[son] pid %d from [parent] pid %d\n",getpid());  
+            exit(0);
+        }else{
+        	printf("signaller %d signalling parent\n", getppid());
+        }
+    } 
+	//for(i; i<3; i++){
+		//fork();
+		// if (fork() == 0){
+		// 	//child
+		// 	execl("signaller.c", buffer);
+		// }else{
+		// 	processes[0] = getpid();
+		// 	printf("signaller %d signalling parent\n", processes[i]);
+		// }
+		
+		// fork();
+		// processes[1] = getpid();
+		// printf("signaller %d signalling parent\n", processes[i]);
+
+		// fork();
+		// processes[2] = getpid();
+		// printf("signaller %d signalling parent\n", processes[i]);
+	//}
+	//printf("[son] pid %d from [parent] pid %d\n",getpid(),getppid()); 
+	
 
 	alarm(resetTimeInSec);
 
@@ -59,5 +117,12 @@ int	main(int argc, char *argv[]){
     	sleep(1);
   	}	
 
+  	// when shouldContinue is 0 or false, send SIGINT to all children
+  	int a = 0;
+  	for(a; a<3; a++){
+  		printf("signaller %d stopping\n", processes[a]);
+		kill(processes[a], SIGINT);
+	}
+  	
   	return(EXIT_SUCCESS);
 }
--- signaller.c
+++ signaller.c
@@ -8,6 +8,8 @@ int maxSignalTime;
 int shouldContinue = 1;
 
 void sigAlarmHandler(int sig){
+  // try
+  kill(getppid(), SIGUSR1);
   alarm(rand() % maxSignalTime + 1);
 }
 
@@ -17,17 +19,28 @@ void sigUSR1Handler(int sig){
 }
 
 int	main(int argc, char *argv[]){
-	
-	maxSignalTime = atoi(argv[1]);
 
-	// set up the struct
-	// install the signal handler
+	// TODO
+	// check to see if we need to take in the 2 parameters from launcher
+	if(argc != 2){
+		printf("Usage: signaller <maxSignalTime> \n");
+		return(EXIT_FAILURE);
+	}
+
+	if(atoi(argv[1]) >= MIN_TIME_IN_SECS){
+		maxSignalTime = atoi(argv[1]);
+	}
+	
+
+	// TODO
+	// two signal handlers, one for SIGAlarm/Sigusr1, other for SIGINT
+
 	struct sigaction act;
 	memset(&act, '\0', sizeof(act));
 	act.sa_handler = sigAlarmHandler;
-	signal(SIGINT, sigAlarmHandler);
+	sigaction(SIGALRM,&act,NULL);
 	act.sa_handler = sigUSR1Handler;
-	signal(SIGINT, sigUSR1Handler);
+	sigaction(SIGUSR1,&act,NULL);
 
 	srand(getpid());
 
